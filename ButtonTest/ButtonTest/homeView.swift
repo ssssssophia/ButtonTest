c0240304edc19ea3a7d4c211498101f2b0e3cc5c
ssssssophia <49021427+ssssssophia@users.noreply.github.com>
2020-03-29 01:14:30 +0800

    first page update

--- ButtonTest/ButtonTest/homeView.swift
+++ ButtonTest/ButtonTest/homeView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 
 struct homeView: View {
     
+    @State var viewState = CGSize.zero
+    @State var viewState2 = CGSize.zero
+    @State var animat = false
+    
     let colorLight : Color = Color("bg-light")
     let colorDark : Color = Color("bg-dark")
     let todayColor : Color = Color("lucky")
@@ -56,6 +60,7 @@ struct homeView: View {
                     
                     ZStack {
                         softCircleView()
+//                            .opacity( self.animat ? 0 : 1)
                         
                         Text("顺遂")
                             .font(.custom("indicator", size: 100))
@@ -77,6 +82,29 @@ struct homeView: View {
                     Spacer()
                     
                 }
+                bottomCard2View()
+                    .offset(x: 0, y: viewState2.height)
+                    .gesture(DragGesture()
+                        .onChanged { value in
+                            self.viewState2 = value.translation
+//                            self.animat = true
+                    }
+                        .onEnded { value in
+                            self.viewState2 = CGSize.zero
+//                            self.animat = false
+                    })
+
+                bottomCardView()
+                    .offset(x: 0, y: viewState.height)
+                    .gesture(DragGesture()
+                        .onChanged { value in
+                            self.viewState = value.translation
+//                            self.animat = true
+                    }
+                        .onEnded { value in
+                            self.viewState = CGSize.zero
+//                            self.animat = false
+                    })
         }
         .edgesIgnoringSafeArea(.all)
     }
@@ -85,5 +113,51 @@ struct homeView: View {
 struct homeView_Previews: PreviewProvider {
     static var previews: some View {
         homeView()
+    }
+}
+
+struct bottomCardView: View {
+    var body: some View {
+        VStack(spacing: 15) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("步道")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(Color("grey-dark"))
+            
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .background(Color.white)
+        .cornerRadius(30)
+        .offset(y: 810)
+        .shadow(radius: 20)
+    }
+}
+
+struct bottomCard2View: View {
+    var body: some View {
+        VStack(spacing: 15) {
+            Rectangle()
+                .frame(width: 60, height: 6)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("万事")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(Color("grey-dark"))
+            
+            Spacer()
+        }
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .padding()
+        .background(Color.white)
+        .cornerRadius(30)
+        .offset(y: 740)
+        .shadow(radius: 20)
     }
 }
